ff79624f765d5967b13e61eb594afa0c54aaf0a2
Sumeet Sahu <thesumeetsahu@gmail.com>
2020-01-24 23:47:47 +0530

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 echo -e "\033[0;Copying img folder to public...\033[0m"
 rsync --progress -r -u img/* public/img
+rsync --progress -r -u *.ico public
 
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 cd public
